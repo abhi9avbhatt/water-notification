9d2acbd5b1bee745614d95f7e878f68b00378fa0
abhi9avbhatt <abhinavdm2@gmail.com>
2026-08-28 15:36:33 +0530

    Add files via upload

---
+++ notification.py
@@ -0,0 +1,10 @@
+
+import time 
+from plyer import notification
+notification.notify(
+    title  = 'DRINK WATER BRO',
+    message = '15.5 cups (3.7 liters) of fluids daily for men ' 
+    ' 11.5 cups (2.7 liters) for women',
+    # app_icon = r'image.ico location',
+    timeout = 5
+)
